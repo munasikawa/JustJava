a7511e005746f1b97cb0227db2f430f3e6d81e0b
munasikawa <munasikawa@gmail.com>
2018-10-16 15:06:35 +0300

    9-19-18

--- app/src/main/java/com/example/hashi/justjava/MainActivity.java
+++ app/src/main/java/com/example/hashi/justjava/MainActivity.java
@@ -148,17 +148,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-
-    private void displayMessage(String message) {
-
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-
-        orderSummaryTextView.setText(message);
-
-    }
-
 
 }
